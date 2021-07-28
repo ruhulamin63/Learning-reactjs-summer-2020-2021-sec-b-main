021446183425204db3270d648054e4e387f34e21
ruhulamin63 <aminr@DESKTOP-4J3MPMA>
2021-07-28 12:23:52 +0600

    croud file

--- task/src/components/CreateUser.js
+++ task/src/components/CreateUser.js
@@ -1,22 +1,71 @@
-const CreateUser = ({createCallback})=>{
-    return(
-        <div>
-           <table>
-                <tr >
-                    <td><label>ID: </label></td>
-                    <td><input type="number" name="id" id="id"/></td>
-                </tr>
-                <tr >
-                    <td><label>Name: </label></td>
-                    <td><input type="text" name="name" id="name"/></td>
-                </tr>
-               <tr>
-                    <td><label>Dept: </label></td>
-                    <td><input type="text" dept="dept" id="dept"/></td>
-                </tr>
-            </table>
-            <button onClick={()=>createCallback()}>Add</button>
-        </div> 
+import {useState} from 'react';
+import {users} from '../usersData';
+import {useParams} from 'react-router-dom';
+
+const CreateUser = ({status, callback})=> {
+
+    const {id:eid}  = useParams();
+    const [newUser, setNewUser] = useState({
+        id: '',
+        name: '',
+        dept: '',
+    });
+
+    const changeUser = (e)=>{
+        const attar = e.target.name;
+        const value = e.target.value;
+        const user = {...newUser, [attar] : value}; 
+        setNewUser(user);
+    }
+
+    const formSubmit=(e)=>{
+        e.preventDefault();
+
+        if(status == 'add'){
+            callback(newUser);
+            setNewUser({
+                id: '',
+                name: '',
+                dept: '',
+            })
+
+        }
+    }
+
+    return (
+        <div className="form">
+            <form onSubmit={formSubmit}>
+                <table>
+                    <thead></thead>
+                    <tbody>
+                        <tr>
+                            <td>ID</td>
+                            <td>
+                                <input type="number" name="id" value={data.id} onChange={changeUser}/> 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Name</td>
+                            <td>
+                                <input type="text" name="name" value={data.name} onChange={changeUser}/> 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Dept</td>
+                            <td>
+                                <input type="text" name="dept" value={data.dept} onChange={changeUser} /> 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>
+                                <button type="submit">Add</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
     );
 }
 
