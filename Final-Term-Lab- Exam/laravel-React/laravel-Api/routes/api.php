f2afc4dd1a08215fb82db39e5ca4b591e21eba5f
ruhulamin63 <aminr@DESKTOP-4J3MPMA>
2021-08-04 10:37:53 +0600

    admin dashboard created

--- Final-Term-Lab- Exam/laravel-React/laravel-Api/routes/api.php
+++ Final-Term-Lab- Exam/laravel-React/laravel-Api/routes/api.php
@@ -4,9 +4,10 @@ use App\Http\Controllers\api\EmpController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/emp-register', [EmpController::class, 'store']);
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/emp-register', [EmpController::class, 'store']);
