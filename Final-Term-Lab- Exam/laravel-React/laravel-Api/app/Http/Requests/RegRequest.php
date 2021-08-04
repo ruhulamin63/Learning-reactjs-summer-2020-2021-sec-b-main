74a7a2a32ad75ceac0cfbbb54491e8f2cf60b156
ruhulamin63 <aminr@DESKTOP-4J3MPMA>
2021-08-04 06:06:06 +0600

    create middleware & request

---
+++ Final-Term-Lab- Exam/laravel-React/laravel-Api/app/Http/Requests/RegRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username'=>'required|min:4',
+            'name'=>'required|min:3|max:30',
+            'email'=>'required|min:5',
+            'password'=>'required|min:8|max:20'
+        ];
+    }
+}
